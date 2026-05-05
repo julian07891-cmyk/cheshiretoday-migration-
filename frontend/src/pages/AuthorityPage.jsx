8928abbafea362c6e314cc6d7b1e07a5a9479871
Iulian Dumitrascu <julian07891@yahoo.com.uk>
2026-05-05 22:23:44 +0100

    Fix top provider CTA tracking guide slug

--- frontend/src/pages/AuthorityPage.jsx
+++ frontend/src/pages/AuthorityPage.jsx
@@ -149,7 +149,7 @@ function BestPickCta({ tools = [], monetisation = "affiliate" }) {
                 target="_blank"
                 rel="noreferrer"
                 onClick={() => trackEvent("guide_provider_click", {
-                  guide: slug,
+                  guide: page?.slug || "",
                   provider: name,
                   position: 1,
                   destination: link,
